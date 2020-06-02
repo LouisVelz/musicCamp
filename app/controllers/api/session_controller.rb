3b4c262ae843d9450ea57816893c1620187d93c4
LouisVelz <velazquezlouis2@gmail.com>
2020-06-01 20:26:04 -0400

    jbuilder files and controllers created

--- app/controllers/api/session_controller.rb
+++ app/controllers/api/session_controller.rb
@@ -1,2 +1,35 @@
 class Api::SessionController < ApplicationController
+      def create
+        @user = User.find_by_credentials(
+            params[:user][:email],
+            # params[:user][:username],
+            params[:user][:password]
+        )
+        unless @user
+          @user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+          )
+        end
+
+        if @user
+            sign_in!(@user)
+            # render '/'
+            # render '/'
+            render :show
+        else
+          render json: ["Invalid credentials!"], status: 404
+          # redirect_to root_url
+        end
+    end
+
+    def destroy
+
+        if current_user
+            sign_out!
+            render json: {}
+        else
+            render json: ["User not signed in"], status: 401
+        end
+    end
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,17 @@
 class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save!
+        sign_in!(@user)
+        # redirect_to root_url
+    else
+        render json: @user.errors.full_message
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :username)
+  end
+  
 end
