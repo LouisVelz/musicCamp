8ad81975f7e9d7de325fc15e8c6b6f8c2ab6f45a
LouisVelz <velazquezlouis2@gmail.com>
2020-07-08 21:16:01 -0400

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,4 +18,107 @@ email: "new_artist@gmail.com",
  description: "Lorem ipsum dolor sit amet,
  consectetur adipisicing elit. Delectus quod, placeat accusamus.")
 
- new_artist_photo = open('')
+ new_artist_photo = open('https://musiccamp-seeds.s3.amazonaws.com/music.jpg')
+ new_artist.photo.attach(io: new_artist_photo, filename: "music.jpeg")
+
+new_artist_1 = User.create(
+email: "new_artist24@gmail.com",
+ password: "hunter12",
+ username: "Singer"
+ description: "Lorem ipsum dolor sit amet,
+ consectetur adipisicing elit. Delectus quod, placeat accusamus.")
+
+ new_artist_photo_1 = open('https://musiccamp-seeds.s3.amazonaws.com/guitar.jpg')
+ new_artist_1.photo.attach(io: new_artist_photo_1, filename: "guitar.jpeg")
+
+new_artist_2 = User.create(
+email: "new_artist54@gmail.com",
+ password: "hunter12",
+ username: "Experienced Musician"
+ description: "Lorem ipsum dolor sit amet,
+ consectetur adipisicing elit. Delectus quod, placeat accusamus.")
+
+ new_artist_photo_2 = open('https://musiccamp-seeds.s3.amazonaws.com/list5.png')
+ new_artist_2.photo.attach(io: new_artist_photo_2, filename: "list5.png")
+
+new_artist_3 = User.create(
+email: "new_artist74@gmail.com",
+ password: "hunter12",
+ username: "Guitar Player Girl"
+ description: "Lorem ipsum dolor sit amet,
+ consectetur adipisicing elit. Delectus quod, placeat accusamus.")
+
+ new_artist_photo_3 = open('https://musiccamp-seeds.s3.amazonaws.com/girl1.jpg')
+ new_artist_3.photo.attach(io: new_artist_photo_3, filename: "girl1.jpg")
+
+new_artist_4 = User.create(
+ email: "new_artist95@gmail.com",
+ password: "hunter12",
+ username: "Guitar Player Guy",
+ description: "Lorem ipsum dolor sit amet,
+ consectetur adipisicing elit. Delectus quod, placeat accusamus.")
+
+ new_artist_photo_4 = open('https://musiccamp-seeds.s3.amazonaws.com/guitar2.jpg')
+ new_artist_4.photo.attach(io: new_artist_photo_4, filename: "guitar2.jpg")
+
+ album01 = Album.create(
+   title: "Lorem Ipsum"
+   description: "Lorem ipsum dolor sit amet,
+                  consectetur adipisicing elit."
+  artist_id: new_artist.id
+ )
+ album01_photo = open('https://musiccamp-seeds.s3.amazonaws.com/girl_radio.jpg')
+ album01.photo.attach(io: album01_photo, filename: "girl_radio.jpg")
+
+ album02 = Album.create(
+   title: "Lorem Ipsum"
+   description: "Lorem ipsum dolor sit amet,
+                  consectetur adipisicing elit."
+  artist_id: new_artist.id
+ )
+
+ album02_photo = open('https://musiccamp-seeds.s3.amazonaws.com/red_lights.jpg')
+ album02.photo.attach(io: album02_photo, filename: "red_lights.jpg")
+
+ album03 = Album.create(
+   title: "Lorem Ipsum"
+   description: "Lorem ipsum dolor sit amet,
+                  consectetur adipisicing elit."
+  artist_id: new_artist.id
+ )
+
+ album03_photo = open('https://musiccamp-seeds.s3.amazonaws.com/you_are_what.jpg')
+ album03.photo.attach(io: album03_photo, filename: "you_are_what.jpg")
+
+ album04 = Album.create(
+   title: "Lorem Ipsum"
+   description: "Lorem ipsum dolor sit amet,
+                  consectetur adipisicing elit."
+  artist_id: new_artist.id
+ )
+
+ album04_photo = open('https://musiccamp-seeds.s3.amazonaws.com/cassette2.jpg')
+ album04.photo.attach(io: album04_photo, filename: "cassette2.jpg")
+
+ album05 = Album.create(
+   title: "Lorem Ipsum"
+   description: "Lorem ipsum dolor sit amet,
+                  consectetur adipisicing elit."
+  artist_id: new_artist.id
+ )
+
+ album05_photo = open('https://musiccamp-seeds.s3.amazonaws.com/headphones.jpg')
+ album05.photo.attach(io: album05_photo, filename: "headphones.jpg")
+
+ album06 = Album.create(
+   title: "Lorem Ipsum"
+   description: "Lorem ipsum dolor sit amet,
+                  consectetur adipisicing elit."
+  artist_id: new_artist.id
+ )
+
+ album06_photo = open('https://musiccamp-seeds.s3.amazonaws.com/headphones.jpg')
+ album06.photo.attach(io: album06_photo, filename: "headphones.jpg")
+
+
+
